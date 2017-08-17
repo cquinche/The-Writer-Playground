9c7ce7f61d3a3faf2e1e466b365d3c10f0ca927a
cquincheASC4 <quinchechrist@gmail.com>
2017-08-17 09:49:02 -0400

    hi

--- js/gameCode.js
+++ js/gameCode.js
@@ -49,7 +49,7 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + /* 60 * */   30000);
+var deadline = new Date(Date.parse(new Date()) + /* 60 * */   15000);
 initializeClock('clockdiv', deadline);
 
 }
